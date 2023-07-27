c47f21a13f9c5ef350c15bacaedd1686036fdce8
priyahatkar <124456200+priyahatkar@users.noreply.github.com>
2023-07-27 14:48:00 +0530

    work on components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ParaComponent } from './para/para.component';
+import { ListComponent } from './list/list.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImagesComponent } from './images/images.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ParaComponent,
+    ListComponent,
+    FooterComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/para/para.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector : "app-para",
+    templateUrl : "./para.component.html",
+    styleUrls : ["./para.component.scss"]
+})
+export class ParaComponent{
+
+}
